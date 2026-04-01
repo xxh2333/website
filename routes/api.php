3a241d8d107e3baca007ad954d192a04fbb1d20b
CGJ-666 <2835129893@qq.com>
2026-04-01 22:27:28 +0800

    .

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\WwjController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,14 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-    Route::post('user/register', [WwjController::class, 'Wwjregister']);
-    Route::post('user/sendVerificationCode', [WwjController::class, 'sendVerificationCode'])->name('send.verification.code');
-    Route::post('user/forgotPassword', [WwjController::class, 'forgotPassword']);
-    Route::get('admin/export-competition-star', [WwjController::class, 'exportCompetitionStar']);
-    Route::get('admin/export-innovation-star', [WwjController::class, 'exportInnovationStar']);
-    Route::get('admin/export-science-star', [WwjController::class, 'exportScienceStar']);
-    Route::post('user/viewScienceStar', [WwjController::class, 'viewScienceStar']);
-//    Route::post('certificateContent', [WwjController::class, 'certificateContent']);
 
 
 
