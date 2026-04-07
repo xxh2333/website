2751d0ec5ab2575c6a2de693e21e10e2e398f13f
xxh2333 <2633681826@qq.com>
2026-04-07 19:26:42 +0800

    修改的迁移文件

---
+++ database/migrations/2026_04_01_205041_create_lab_info_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('lab_info', function (Blueprint $table) {
+            $table->id()->comment('主键ID');
+            $table->string('name', 100)->comment('实验室名称');
+            $table->text('desc')->comment('实验室简介');
+            $table->string('address')->nullable()->comment('实验室地址');
+            $table->string('contact')->nullable()->comment('联系方式');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('lab_info');
+    }
+};
